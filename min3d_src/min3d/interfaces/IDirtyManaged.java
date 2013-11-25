c7e33488cabba1f012b4a999bf640dac7cb07130
mengdd <mengdandanno1@163.com>
2013-11-26 01:04:55 +0800

    move min3d src

--- min3d_src/min3d/interfaces/IDirtyManaged.java
+++
@@ -1,8 +0,0 @@
-package min3d.interfaces;
-
-public interface IDirtyManaged 
-{
-	public boolean isDirty();
-	public void setDirtyFlag();
-	public void clearDirtyFlag();
-}
--- min3d_src/min3d/interfaces/IDirtyParent.java
+++
@@ -1,6 +0,0 @@
-package min3d.interfaces;
-
-public interface IDirtyParent 
-{
-	public void onDirty();
-}
