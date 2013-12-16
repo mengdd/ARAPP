bc145ed9ceb2b730b541115e8f445d962667d302
mengdd <mengdd23@gmail.com>
2013-12-16 18:10:37 +0800

    new log util class

--- src/com/mengdd/utils/LogUtils.java
+++ src/com/mengdd/utils/LogUtils.java
@@ -1,68 +1,200 @@
 package com.mengdd.utils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import android.os.Environment;
+import android.text.TextUtils;
 import android.util.Log;
 
 public class LogUtils {
 
-    private static boolean debug = true;
+    private static boolean DEBUG = true;
+    private static String LOG_TAG = "mengdd";
 
-    public static String getTrackInfo() {
-        StackTraceElement element = Thread.currentThread().getStackTrace()[4];
-        String className = element.getClassName();
-        String method = element.getMethodName();
-        return className + "-->" + method;
-    }
+    private final static boolean LOG_TO_FILE = true;
+    private final static String LOG_FILE_PATH = Environment
+            .getExternalStorageDirectory().getPath() + "/mengdd_debug_log.txt";
 
-    public static void v(String tag, String msg) {
-        if (debug) {
-            Log.v(tag, msg);
+    private static File LOG_FILE;
+
+    public static void footPrint() {
+        if (DEBUG) {
+            String className = Thread.currentThread().getStackTrace()[3]
+                    .getClassName();
+            int index = className.lastIndexOf(".");
+            if (index > -1) {
+                className = className.substring(index + 1);
+            }
+            String msgToPrint = Thread.currentThread().getId() + " "
+                    + className + "."
+                    + Thread.currentThread().getStackTrace()[3].getMethodName();
+            println(Log.DEBUG, LOG_TAG, msgToPrint);
         }
     }
 
-    public static void v(String msg) {
-        v(getTrackInfo(), msg);
+    public static void footPrint(String tag) {
+        if (DEBUG) {
+            String msgToPrint = Thread.currentThread().getStackTrace()[3]
+                    .getMethodName();
+            println(Log.DEBUG, tag, msgToPrint);
+        }
     }
 
-    public static void d(String tag, String msg) {
-        if (debug) {
-            Log.d(tag, msg);
+    public static String getStackTraceString(Throwable tr) {
+        return Log.getStackTraceString(tr);
+    }
+
+    public static void v(String tag, String msg) {
+        if (DEBUG) {
+            println(Log.VERBOSE, tag, msg);
+        }
+    }
+
+    public static void v(String tag, String msg, Throwable tr) {
+        if (DEBUG) {
+            println(Log.VERBOSE, tag, msg + '\n' + getStackTraceString(tr));
         }
     }
 
     public static void d(String msg) {
+        if (DEBUG) {
+            String className = Thread.currentThread().getStackTrace()[3]
+                    .getClassName();
+            int index = className.lastIndexOf(".");
+            if (index > -1) {
+                className = className.substring(index + 1);
+            }
 
-        d(getTrackInfo(), msg);
-
+            String msgToPrint = Thread.currentThread().getId() + " "
+                    + className + "."
+                    + Thread.currentThread().getStackTrace()[3].getMethodName();
+            if (!TextUtils.isEmpty(msg)) {
+                msgToPrint += "--" + msg;
+            }
+            println(Log.DEBUG, LOG_TAG, msgToPrint);
+        }
     }
 
-    public static void i(String tag, String msg) {
-        if (debug) {
-            Log.i(tag, msg);
+    public static void d(String tag, String msg) {
+        if (DEBUG) {
+            String msgToPrint = Thread.currentThread().getStackTrace()[3]
+                    .getMethodName();
+            if (!TextUtils.isEmpty(msg)) {
+                msgToPrint += "--" + msg;
+            }
+            println(Log.DEBUG, tag, msgToPrint);
+        }
+    }
+
+    public static void d(String tag, String msg, Throwable tr) {
+        if (DEBUG) {
+            println(Log.DEBUG, tag, msg + '\n' + getStackTraceString(tr));
         }
     }
 
     public static void i(String msg) {
-        i(getTrackInfo(), msg);
+        if (DEBUG) {
+            String className = Thread.currentThread().getStackTrace()[3]
+                    .getClassName();
+            int index = className.lastIndexOf(".");
+            if (index > -1) {
+                className = className.substring(index + 1);
+            }
 
-    }
-
-    public static void w(String tag, String msg) {
-        if (debug) {
-            Log.w(tag, msg);
+            String msgToPrint = Thread.currentThread().getId() + " "
+                    + className + "."
+                    + Thread.currentThread().getStackTrace()[3].getMethodName();
+            if (!TextUtils.isEmpty(msg)) {
+                msgToPrint += "--" + msg;
+            }
+            println(Log.INFO, LOG_TAG, msgToPrint);
         }
     }
 
-    public static void w(String msg) {
-        w(getTrackInfo(), msg);
-    }
-
-    public static void e(String tag, String msg) {
-        if (debug) {
-            Log.e(tag, msg);
+    public static void i(String tag, String msg) {
+        if (DEBUG) {
+            String msgToPrint = Thread.currentThread().getStackTrace()[3]
+                    .getMethodName();
+            msgToPrint += "--" + msg;
+            println(Log.INFO, tag, msgToPrint);
         }
     }
 
-    public static void e(String msg) {
-        e(getTrackInfo(), msg);
+    public static void i(String tag, String msg, Throwable tr) {
+        if (DEBUG) {
+            println(Log.INFO, tag, msg + '\n' + getStackTraceString(tr));
+        }
+    }
+
+    public static int w(String tag, String msg) {
+        return println(Log.WARN, tag, msg);
+    }
+
+    public static int w(String tag, String msg, Throwable tr) {
+        return println(Log.WARN, tag, msg + '\n' + getStackTraceString(tr));
+    }
+
+    public static int w(String tag, Throwable tr) {
+        return println(Log.WARN, tag, getStackTraceString(tr));
+    }
+
+    public static int e(String msg) {
+        return println(Log.ERROR, LOG_TAG, msg);
+    }
+
+    public static int e(String tag, String msg) {
+        return println(Log.ERROR, tag, msg);
+    }
+
+    public static int e(String tag, String msg, Throwable tr) {
+        return println(Log.ERROR, tag, msg + '\n' + getStackTraceString(tr));
+    }
+
+    public static int wtf(String tag, String msg) {
+        return Log.wtf(tag, msg, null);
+    }
+
+    public static int wtf(String tag, Throwable tr) {
+        return Log.wtf(tag, tr.getMessage(), tr);
+    }
+
+    public static int wtf(String tag, String msg, Throwable tr) {
+        return Log.wtf(tag, msg, tr);
+    }
+
+    public static int println(int priority, String tag, String msg) {
+        if (TextUtils.isEmpty(msg)) {
+            msg = "";
+        }
+        if (LOG_TO_FILE) {
+            logToFile(tag, msg);
+            return 0;
+        } else {
+            return Log.println(priority, tag, msg);
+        }
+    }
+
+    private static synchronized void logToFile(String tag, String msg) {
+        try {
+            if (LOG_FILE == null) {
+                LOG_FILE = new File(LOG_FILE_PATH);
+                if (LOG_FILE.exists()) {
+                    LOG_FILE.delete();
+                }
+                LOG_FILE.createNewFile();
+            }
+
+            FileOutputStream outputStream = new FileOutputStream(LOG_FILE, true);
+
+            StringBuilder builder = new StringBuilder();
+            builder.append(tag).append("  :  ").append(msg).append("\n");
+
+            outputStream.write(builder.toString().getBytes());
+            outputStream.flush();
+            outputStream.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
